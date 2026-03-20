cdda75d5c9663704a061569b6f2229d07182210c
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-03-20 15:46:48 -0300

    primer commit

---
+++ src/main/java/cl/bancox/saldo/SaldoApplication.java
@@ -0,0 +1,13 @@
+package cl.bancox.saldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaldoApplication.class, args);
+	}
+
+}
